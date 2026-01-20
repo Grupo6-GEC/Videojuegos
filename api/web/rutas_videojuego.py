44dfb3bfa79319eb6973ad0cb67f6d102d60c547
Grupo6-GEC <carla.ledesma1@educa.madrid.org>
2026-01-20 19:58:59 +0100

    Update and rename rutas_chuches.py to rutas_videojuego.py

---
+++ api/web/rutas_videojuego.py
@@ -0,0 +1,65 @@
+from flask import request, Blueprint, jsonify
+import controlador_videojuegos
+from funciones_auxiliares import Encoder
+
+bp = Blueprint('videojuego', __name__)
+
+@bp.route("/",methods=["GET"])
+def videojuegos():
+    respuesta,code= controlador_videojuegos.obtener_videojuegos()
+    return jsonify(respuesta), code
+
+###
+
+@bp.route("/<id>",methods=["GET"])
+def videojuego_por_id(id):
+    respuesta,code = controlador_videojuegos.obtener_videojuego_por_id(id)
+    return jsonify(respuesta), code
+
+###
+
+@bp.route("/",methods=["POST"])
+def guardar_videojuego():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        videojuego_json = request.json
+        nombre = videojuego_json["nombre"]
+        foto=videojuego_json["foto"]
+        descripcion = videojuego_json["descripcion"]
+        precio=videojuego_json["precio"]
+        creador=videojuego_json["creador"]
+        ruta_foto=videojuego_json["ruta_foto"]
+        tienda=videojuego_json["tienda"]
+        respuesta,code=controlador_videojuegos.insertar_videojuego(nombre, foto, descripcion, precio, creador, ruta_foto, tienda)
+    else:
+        respuesta={"status":"Bad request"}
+        code=400
+    return jsonify(respuesta), code
+
+####
+
+@bp.route("/<int:id>", methods=["DELETE"])
+def eliminar_videojuego(id):
+    respuesta,code=controlador_videojuegos.eliminar_videojuego(id)
+    return jsonify(respuesta), code
+
+###
+
+@bp.route("/", methods=["PUT"])
+def actualizar_videojuego():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        videojuego_json = request.json
+        id = videojuego_json["id"]
+        nombre = videojuego_json["nombre"]
+        foto=videojuego_json["foto"]
+        descripcion = videojuego_json["descripcion"]
+        precio=float(videojuego_json["precio"])
+        creador=videojuego_json["creador"]
+        ruta_foto=videojuego_json["ruta_foto"]
+        tienda=videojuego_json["tienda"]
+        respuesta,code=controlador_videojuegos.actualizar_videojuego(id, nombre, foto, descripcion, precio, creador, ruta_foto, tienda)
+    else:
+        respuesta={"status":"Bad request"}
+        code=400
+    return jsonify(respuesta), code
