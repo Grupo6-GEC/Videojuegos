fcce85bf5e4424bf2913d85b9776ecc7e001842a
Grupo6-GEC <carla.ledesma1@educa.madrid.org>
2026-02-18 13:30:50 +0100

    Update test_calculariva.py
    definicion de nombres correcto

--- tests/test_calculariva.py
+++ tests/test_calculariva.py
@@ -2,4 +2,4 @@ from api.web.funciones_auxiliares import calculariva
 def test_calculariva100():
     resultado_esperado = 21
     resultado = calculariva(100)
-    assert resultado == resultado_esperdo
+    assert resultado == resultado_esperado
