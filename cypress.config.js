0ab34c115bad749c973c080635f78f868b4fda1c
Grupo6-GEC <carla.ledesma1@educa.madrid.org>
2026-03-09 11:56:56 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
 
-    baseUrl: "http://localhost:9081",
+    baseUrl: "http://localhost:6106",
 
     supportFile: false
 
